92f0e8de32fe6bf61b9bbccbe76a54a32aa0b6dd
César <cesardsbraz@gmail.com>
2024-10-02 12:39:02 -0300

    :lipstick: feat: Estilização da tela de quadrinhos

--- src/pages/Comics.tsx
+++ src/pages/Comics.tsx
@@ -2,7 +2,7 @@ import ComicCard from "../components/ComicCard"
 
 export default function Comics() {
   return (
-    <div>
+    <div className="flex flex-wrap justify-evenly m-8 px-28">
       <ComicCard title="The Amazing Spider Man #18" price="34,90" author="William, Turg" year={2023}/>
     </div>
   )
