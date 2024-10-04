8de6ba4c3cce5a02f171bcb035c23f7607bcd2e4
César <cesardsbraz@gmail.com>
2024-10-04 16:07:25 -0300

    :construction: feat: Criado card de item e iniciando os contextos

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,7 +1,20 @@
+import CartItemCard from "../components/CartItemCard";
+import { useCart } from "../context/useShoppingCart";
+
 export default function ShoppingCart() {
+  const cart = useCart()
+
   return (
-    <div>
-      Carrinho de Compras
+    <div className="flex flex-col items-center">
+      <h1 className="text-center font-extrabold text-[32px] sm:text-6xl my-8 sm:mt-8">Meu Carrinho</h1>
+      {cart.items.map(item => (
+        <CartItemCard
+          key={item.id}
+          img={item.img}
+          title={item.img}
+          price={item.price}
+        />
+      ))}
     </div>
   )
 }
